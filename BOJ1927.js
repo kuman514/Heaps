028c122da15418cecdd3a934d3ff5ae6bcb70377
kumankoishi.py <7iggif@naver.com>
2021-08-30 23:34:28 +0900

    Testing my heap in JS

---
+++ BOJ1927.js
@@ -0,0 +1,140 @@
+// Testing heap using BOJ 1927
+
+class Heap {
+  constructor(compareFunction) {
+    // return of the compare function
+    // lower than 0: lhs is prior
+    // higher than 0: rhs is prior
+    // exactly 0: same priority
+
+    this.compareFunction = compareFunction;
+    this.tree = [null];
+
+    this.push = (val) => {
+      let curIndex = this.tree.length;
+      this.tree.push(val);
+
+      while (curIndex > 1) {
+        const parent = Math.floor(curIndex / 2);
+        if (this.compareFunction(this.tree[curIndex], this.tree[parent]) < 0) {
+          const swapTmp = this.tree[curIndex];
+          this.tree[curIndex] = this.tree[parent];
+          this.tree[parent] = swapTmp;
+          curIndex = parent;
+        } else {
+          break;
+        }
+      }
+    }
+
+    this.pop = () => {
+      if (this.tree.length <= 1) {
+        return null;
+      }
+
+      if (this.tree.length === 2) {
+        return this.tree.pop();
+      } else if (this.tree.length > 2) {
+        const returnVal = this.tree[1];
+        this.tree[1] = this.tree.pop();
+
+        let curIndex = 1;
+        while (curIndex < this.tree.length) {
+          const lchild = curIndex * 2;
+          const rchild = curIndex * 2 + 1;
+          let continuable = true;
+
+          if (lchild >= this.tree.length) {
+            continuable = false;
+          } else if (rchild >= this.tree.length) {
+            if (this.compareFunction(this.tree[curIndex], this.tree[lchild]) > 0) {
+              const swapTmp = this.tree[curIndex];
+              this.tree[curIndex] = this.tree[lchild];
+              this.tree[lchild] = swapTmp;
+              curIndex = lchild;
+            } else {
+              continuable = false;
+            }
+          } else {
+            const priorL = this.compareFunction(this.tree[curIndex], this.tree[lchild]);
+            const priorR = this.compareFunction(this.tree[curIndex], this.tree[rchild]);
+
+            if (priorL > 0 && priorR <= 0) {
+              const swapTmp = this.tree[curIndex];
+              this.tree[curIndex] = this.tree[lchild];
+              this.tree[lchild] = swapTmp;
+              curIndex = lchild;
+            } else if (priorL <= 0 && priorR > 0) {
+              const swapTmp = this.tree[curIndex];
+              this.tree[curIndex] = this.tree[rchild];
+              this.tree[rchild] = swapTmp;
+              curIndex = rchild;
+            } else if (priorL > 0 && priorR > 0) {
+              const priorBoth = this.compareFunction(this.tree[lchild], this.tree[rchild]);
+              if (priorBoth <= 0) {
+                const swapTmp = this.tree[curIndex];
+                this.tree[curIndex] = this.tree[lchild];
+                this.tree[lchild] = swapTmp;
+                curIndex = lchild;
+              } else {
+                const swapTmp = this.tree[curIndex];
+                this.tree[curIndex] = this.tree[rchild];
+                this.tree[rchild] = swapTmp;
+                curIndex = rchild;
+              }
+            } else if (priorL <= 0 && priorR <= 0) {
+              continuable = false;
+            }
+          }
+
+          if (!continuable) {
+            break;
+          }
+        }
+
+        return returnVal;
+      }
+    }
+  }
+}
+
+const readLine = require('readline');
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let count = null;
+const commands = [];
+let result = '';
+
+rl.on('line', (line) => {
+  if (count === null) {
+    count = parseInt(line);
+  } else {
+    commands.push(parseInt(line));
+    count--;
+    if (count <= 0) {
+      testHeap();
+      console.log(result);
+      rl.close();
+    }
+  }
+}).on('close', () => {
+  process.exit();
+});
+
+function testHeap() {
+  const heap = new Heap((a, b) => (a - b));
+  for (const num of commands) {
+    switch (num) {
+      case 0:
+        const tmp = heap.pop();
+        result += `${(tmp === null) ? 0 : tmp}\n`;
+        break;
+      default:
+        heap.push(num);
+        break;
+    }
+  }
+}
