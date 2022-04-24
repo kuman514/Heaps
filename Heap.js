121be0ab2def907b00c16e230de9fee8f9b42fdf
kumankoishi.py <7iggif@naver.com>
2022-04-24 22:49:16 +0900

    Updated Heap with array encapsulation

--- Heap.js
+++ Heap.js
@@ -1,97 +1,73 @@
 class Heap {
-  constructor(compareFunction) {
-    // return of the compare function
-    // lower than 0: lhs is prior
-    // higher than 0: rhs is prior
-    // exactly 0: same priority
-
-    this.compareFunction = compareFunction;
-    this.tree = [null];
-
-    this.push = (val) => {
-      let curIndex = this.tree.length;
-      this.tree.push(val);
-
+  constructor(compare = (a, b) => (b - a)) {
+    const heapArray = [];
+    const compareFunc = compare;
+    
+    this.push = (value) => {
+      heapArray.push(value);
+      let curIndex = heapArray.length;
       while (curIndex > 1) {
-        const parent = Math.floor(curIndex / 2);
-        if (this.compareFunction(this.tree[curIndex], this.tree[parent]) < 0) {
-          const swapTmp = this.tree[curIndex];
-          this.tree[curIndex] = this.tree[parent];
-          this.tree[parent] = swapTmp;
-          curIndex = parent;
-        } else {
+        const parentIndex = Math.floor(curIndex / 2);
+        const [realCurIndex, realParentIndex] = [curIndex - 1, parentIndex - 1];
+        if (compareFunc(heapArray[realCurIndex], heapArray[realParentIndex]) >= 0) {
           break;
         }
+        [heapArray[realCurIndex], heapArray[realParentIndex]] = [heapArray[realParentIndex], heapArray[realCurIndex]];
+        curIndex = parentIndex;
       }
-    }
-
-    this.pop = () => {
-      if (this.tree.length <= 1) {
+    };
+    
+    this.top = () => {
+      if (heapArray.legnth <= 0) {
         return null;
       }
-
-      if (this.tree.length === 2) {
-        return this.tree.pop();
-      } else if (this.tree.length > 2) {
-        const returnVal = this.tree[1];
-        this.tree[1] = this.tree.pop();
-
-        let curIndex = 1;
-        while (curIndex < this.tree.length) {
-          const lchild = curIndex * 2;
-          const rchild = curIndex * 2 + 1;
-          let continuable = true;
-
-          if (lchild >= this.tree.length) {
-            continuable = false;
-          } else if (rchild >= this.tree.length) {
-            if (this.compareFunction(this.tree[curIndex], this.tree[lchild]) > 0) {
-              const swapTmp = this.tree[curIndex];
-              this.tree[curIndex] = this.tree[lchild];
-              this.tree[lchild] = swapTmp;
-              curIndex = lchild;
-            } else {
-              continuable = false;
-            }
-          } else {
-            const priorL = this.compareFunction(this.tree[curIndex], this.tree[lchild]);
-            const priorR = this.compareFunction(this.tree[curIndex], this.tree[rchild]);
-
-            if (priorL > 0 && priorR <= 0) {
-              const swapTmp = this.tree[curIndex];
-              this.tree[curIndex] = this.tree[lchild];
-              this.tree[lchild] = swapTmp;
-              curIndex = lchild;
-            } else if (priorL <= 0 && priorR > 0) {
-              const swapTmp = this.tree[curIndex];
-              this.tree[curIndex] = this.tree[rchild];
-              this.tree[rchild] = swapTmp;
-              curIndex = rchild;
-            } else if (priorL > 0 && priorR > 0) {
-              const priorBoth = this.compareFunction(this.tree[lchild], this.tree[rchild]);
-              if (priorBoth <= 0) {
-                const swapTmp = this.tree[curIndex];
-                this.tree[curIndex] = this.tree[lchild];
-                this.tree[lchild] = swapTmp;
-                curIndex = lchild;
-              } else {
-                const swapTmp = this.tree[curIndex];
-                this.tree[curIndex] = this.tree[rchild];
-                this.tree[rchild] = swapTmp;
-                curIndex = rchild;
-              }
-            } else if (priorL <= 0 && priorR <= 0) {
-              continuable = false;
-            }
-          }
-
-          if (!continuable) {
+      return heapArray[0];
+    };
+    
+    this.pop = () => {
+      if (heapArray.length <= 0) {
+        return null;
+      }
+      if (heapArray.length === 1) {
+        return heapArray.pop();
+      }
+      const valueToReturn = heapArray[0];
+      heapArray[0] = heapArray.pop();
+      let curIndex = 1;
+      while (curIndex * 2 <= heapArray.length) {
+        const [leftIndex, rightIndex] = [curIndex * 2, curIndex * 2 + 1];
+        const [realCurIndex, realLeftIndex, realRightIndex] = [curIndex - 1, leftIndex - 1, rightIndex - 1];
+        const [noLowerThanLeft, noLowerThanRight] = [
+          compareFunc(heapArray[realLeftIndex], heapArray[realCurIndex]) >= 0,
+          compareFunc(heapArray[realRightIndex], heapArray[realCurIndex]) >= 0,
+        ];
+        if (leftIndex <= heapArray.length && rightIndex > heapArray.length) {
+          if (noLowerThanLeft) {
             break;
           }
+          [heapArray[realCurIndex], heapArray[realLeftIndex]] = [heapArray[realLeftIndex], heapArray[realCurIndex]];
+          curIndex = leftIndex;
+          continue;
         }
-
-        return returnVal;
+        if (noLowerThanLeft && noLowerThanRight) {
+          break;
+        } else if (!noLowerThanLeft && noLowerThanRight) {
+          [heapArray[realCurIndex], heapArray[realLeftIndex]] = [heapArray[realLeftIndex], heapArray[realCurIndex]];
+          curIndex = leftIndex;
+        } else if (noLowerThanLeft && !noLowerThanRight) {
+          [heapArray[realCurIndex], heapArray[realRightIndex]] = [heapArray[realRightIndex], heapArray[realCurIndex]];
+          curIndex = rightIndex;
+        } else {
+          if (compareFunc(heapArray[realLeftIndex], heapArray[realRightIndex]) <= 0) {
+            [heapArray[realCurIndex], heapArray[realLeftIndex]] = [heapArray[realLeftIndex], heapArray[realCurIndex]];
+            curIndex = leftIndex;
+          } else {
+            [heapArray[realCurIndex], heapArray[realRightIndex]] = [heapArray[realRightIndex], heapArray[realCurIndex]];
+            curIndex = rightIndex;
+          }
+        }
       }
-    }
+      return valueToReturn;
+    };
   }
 }
